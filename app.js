fb4fec72274d901b79d3a560fcee2c388f96d704
Nesandu <nesanduirosha8@gmail.com>
2020-06-29 08:42:24 +0530

    edited

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let canvas;
 function setup(){
      canvas=createCanvas(windowWidth/2, windowHeight/2);
      canvas.position(windowWidth/4,windowHeight/4);
+     background(0);
 }
 function draw(){
 
