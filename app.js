a6d2fab7cbfa0668dcca1e11f8efbd2cb683c74a
Nesandu <nesanduirosha8@gmail.com>
2020-06-29 08:55:20 +0530

    editted

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 new p5();
 let canvas;
 function setup(){
-     canvas=createCanvas(windowWidth/2, windowHeight/2);
-     canvas.position(windowWidth/4,windowHeight/4);
+     canvas=createCanvas(windowWidth, windowWidth/2);
+     canvas.parent("canvas");
      background(0);
 }
 function draw(){
